730a746b6d042927367f6da6106e89aaa19be65a
Aditiya singh <adisingh20012110@gmail.com>
2024-04-27 20:45:46 +0530

    bg changer react

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/BG-Changer-React/",
   plugins: [react()],
-  base: "/BG-Changer-React/"
 })
